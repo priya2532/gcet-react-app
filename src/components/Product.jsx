555677d306a520116854a8365534b634680c5c6a
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-05 11:29:55 +0530

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,23 +2,31 @@ import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
 import axios from "axios";
+import "./Product.css"
 export default function Product() {
   const { user } = useContext(AppContext);
-  const [product, setProduct] = useState([]);
-  const API=import.meta.env.VITE_API_URL;
-  const fetchProduct = async () => {
-    const res = await axios.get(`${API}/product`);
-    setProduct(res.data.data);
+  const [products, setProducts] = useState([]);
+  const API = import.meta.env.VITE_API_URL;
+  const fetchProducts = async () => {
+    const res = await axios.get(`${API}/products/all`);
+    setProducts(res.data);
   };
   useEffect(() => {
-    fetchProduct();
+    fetchProducts();
   }, []);
-
   return (
     <div>
       <h3>Welcome {user.name}! </h3>
-      Product List
-      {product && product.map((value) => <li>{value.name}- ₹{value.price}</li>)}
+      <div className="App-Product-Row">
+        {products &&
+          products.map((value) => (
+            <div key={value._id}>
+              <h3>{value.name}</h3>
+              <h4>{value.price}</h4>
+              <button>Add to Cart</button>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }

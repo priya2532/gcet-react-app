a539daedece6ac81c53ef5ecd8e1ca3b3b5a38e9
priya2532 <23r11a66b3@gcet.edu.in>
2025-06-05 15:08:01 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import { AppContext } from "../App";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function Login() {
   const { users, user, setUser } = useContext(AppContext);
   const [msg, setMsg] = useState();
   const Navigate = useNavigate();
-  const handleSubmit = () => {
-    const found = users.find(
-      (value) => value.email === user.email && value.pass === user.pass
-    );
-    if (found) {
-      setMsg("Welcome " + found.name);
-      setUser({ ...user, name: found.name, token: "123" });
+  const API = import.meta.env.VITE_API_URL;
+  const handleSubmit = async () => {
+    // const found = users.find(
+    //   (value) => value.email === user.email && value.pass === user.pass
+    // );
+    const url = `${API}/users/login`;
+    const found = await axios.post(url, user);
+    console.log(found)
+
+    if (found.data.email) {
+      setUser(found.data);
       Navigate("/");
     } else {
       setMsg("Invalid User or Password");
